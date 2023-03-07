d600d310fe1f204fc5b4e5de837c4260a5fd443d
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-07 08:27:50 +0530

    Add : EmployeeWageComputationProblem5 is added

---
+++ EmployeeWageComputationProblemuc5.sh
@@ -0,0 +1,18 @@
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ]
+then
+echo "Employee is present"
+else
+echo "Employee is absent"
+fi
+hours=8
+wage_per_hour=20
+daily_wage=$((hours * wage_per_hour))
+
+echo "Daily wage of the employee is :" $daily_wage
+monthly_wages=$((daily_wage * 20))
+
+echo "Monthly wage of the employee is :" $monthly_wage
+
